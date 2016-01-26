ea34bb9b836396a130453e72c93098fae759efdd
cuibing <cb12737@ly.com>
2016-01-26 11:18:58 +0800

    Initial Commit

---
+++ 画画板/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  画画板
+//
+//  Created by cb on 16/1/26.
+//  Copyright © 2016年 cuibing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 画画板/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  画画板
+//
+//  Created by cb on 16/1/26.
+//  Copyright © 2016年 cuibing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
